1fef47a890751a332c7360ea50d024e59ff4d853
Harianto van Insulinde <harianto.v.insulinde@valtech.nl>
2018-10-31 17:34:46 +0100

    Prefix FIX

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
+  prefix: '/css',
+  src: path.join(__dirname, 'src/scss'),
+  dest: path.join(__dirname, 'public/css'),
   indentedSyntax: false, // true = .sass and false = .scss
   sourceMap: true
 }));
